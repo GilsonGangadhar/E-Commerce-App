5230f5b260679be24aff99adf2568a3d7a6007bd
GilsonGangadhar <gilsongangadhar@gmail.com>
2021-10-07 16:10:46 +0530

    building banner section & styling it

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,19 @@ function Header() {
             </div>
 
             {/* bottom nav */}
-            <div></div>
+            <div className="flex items-center bg-amazon_blue-light text-white text-sm space-x-3 p-2 pl-6">
+
+                <p className="link flex items-center">
+                    <MenuIcon className="h-6 mr-1" />
+                    All
+                </p>
+
+                <p className="link">Prime video</p>
+                <p className="link">Amazon business</p>
+                <p className="link">Today's deals</p>
+
+                <p className="link hidden lg:inline-flex">Electronics</p>
+            </div>
         </header>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,26 @@
 import Head from 'next/head'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
+import ProductFeed from '../components/ProductFeed'
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon build</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* header */}
       <Header/>
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* banner */}
+
+        <Banner />
+
+        {/* product feed */}
+
+        <ProductFeed />
+      </main>
     </div>
   )
 }
