439718d3e8943370992ee8d7af538a4a748555b9
GilsonGangadhar <gilsongangadhar@gmail.com>
2021-10-07 17:24:31 +0530

    product listing & styling

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,7 +1,41 @@
-function ProductFeed() {
+import Product from "./Product"
+
+function ProductFeed({products}) {
+    console.log(products, "products")
+
     return (
-        <div>
+        <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
             
+            {
+                products?.slice(0,4).map((product,i) => ( 
+                    <Product
+                    key={i}
+                    product={product}
+                    />
+                ))
+            }
+
+            <img className="md:col-span-full" src="https://links.papareact.com/dyz" alt=""/>
+
+            <div className="md:col-span-2">
+            {
+                products?.slice(4,5).map((product,i) => ( 
+                    <Product
+                    key={i}
+                    product={product}
+                    />
+                ))
+            }
+            </div>
+
+            {
+                products?.slice(5, products.length).map((product,i) => ( 
+                    <Product
+                    key={i}
+                    product={product}
+                    />
+                ))
+            }
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import Banner from '../components/Banner'
 import Header from '../components/Header'
 import ProductFeed from '../components/ProductFeed'
 
-export default function Home() {
+
+
+export default function Home({products}) {
   return (
     <div className="bg-gray-100">
       <Head>
@@ -19,8 +21,15 @@ export default function Home() {
 
         {/* product feed */}
 
-        <ProductFeed />
+        <ProductFeed products={products} />
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  )
+  return { props : {products}}
 }
